dc2d8cc57f164efede2f1b85bd494e0132423add
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-02-18 00:06:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Ane - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Ane;
+
+# DESCRIPTION
+
+Acme::Ane is ...
+
+# LICENSE
+
+Copyright (C) Tomohiro Nishimura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tomohiro Nishimura <tomohiro68@gmail.com>
---
+++ lib/Acme/Ane.pm
@@ -0,0 +1,39 @@
+package Acme::Ane;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Ane - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Ane;
+
+=head1 DESCRIPTION
+
+Acme::Ane is ...
+
+=head1 LICENSE
+
+Copyright (C) Tomohiro Nishimura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tomohiro Nishimura E<lt>tomohiro68@gmail.comE<gt>
+
+=cut
+
